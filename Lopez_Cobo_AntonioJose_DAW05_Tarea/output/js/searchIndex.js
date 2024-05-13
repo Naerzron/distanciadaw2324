9a67ac7f03bb42d13dc3c1a62aaccd946a2bd744
Antonio José López Cobo <ajlopezcobo@outlook.es>
2024-05-13 21:36:53 +0200

    Proyecto LopezCoboAntonioJose2223 comentado con JavaDoc

---
+++ Lopez_Cobo_AntonioJose_DAW05_Tarea/output/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Esta\u0020funci\u00F3n\u0020suma\u0020dos\u0020n\u00FAmeros.",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\multiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "Esta\u0020funci\u00F3n\u0020multiplica\u0020dos\u0020n\u00FAmeros.",
+            "url": "namespaces/default.html#function_multiplicar"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
